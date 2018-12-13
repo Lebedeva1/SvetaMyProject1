50863e6c523d9a5c673d2821b81e0c16ffca3a9d
Lebedeva Sveta <sveta110011@yandex.ru>
2018-12-14 02:49:52 +0300

    add function MyDiv()

--- program.cpp
+++ program.cpp
@@ -1,3 +1,5 @@
 //פאיכ program.cpp #include "stdafx.h" #include <iostream> #include "funcs.h" using namespace std; 
  
-int main() {  cout << MySum(10, 3) << endl;  cout << MySub(10, 3) << endl;  cout << MyMul(10, 3) << endl;  system("pause");  return 0; } 
+int main() {  cout << MySum(10, 3) << endl;  cout << MySub(10, 3) << endl;  cout << MyMul(10, 3) 
+double MyDiv(double a, double b) {  return a / b; } 
+<< endl;  system("pause");  return 0; } 
